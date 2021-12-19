f759087632163347e4f0c8ae620b35de5b7a51ef
sayma078 <somayasayma2@gmail.com>
2021-12-19 20:23:08 +0600

    review

--- index.js
+++ index.js
@@ -1,96 +1,112 @@
-const express = require('express')
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
-const ObjectId = require('mongodb').ObjectId;
-const cors = require('cors')
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.31irp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("tour");
+    const servicesCollection = database.collection("services");
+    const bookingCollection = database.collection("booking");
+    const reviewCollection = database.collection("reviews");
 
-async function run(){
-    try{
-        await client.connect();
-        const database = client.db('tour');
-        const servicesCollection = database.collection('services')
-        const bookingCollection = database.collection('booking')
+    //Get API
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-        //Get API
-        app.get('/services', async(req, res) =>{
-            const cursor = servicesCollection.find({});
-            const services = await cursor.toArray();
-            res.send(services);
-        })
+    //single service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const details = await servicesCollection.findOne(query);
+      res.json(details);
+    });
 
-        //single service
-       app.get('/services/:id', async(req, res)=>{
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const details = await servicesCollection.findOne(query);
-        res.json(details);
-       })
+    //post API
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      console.log("hit the api", service);
 
-        //post API
-        app.post('/services', async(req, res) =>{
-            const service = req.body;
-            console.log('hit the api', service)
-          
-            const result = await servicesCollection.insertOne(service);
-            console.log(result);
-            res.json(result);
-        })
+      const result = await servicesCollection.insertOne(service);
+      console.log(result);
+      res.json(result);
+    });
 
-        //booking post API
-        app.post('/booking', async(req, res) =>{
-            const booking = req.body;
-            console.log('hit the api', booking)
-          
-            const result = await bookingCollection.insertOne(booking);
-            console.log(result);
-            res.json(result);
-        })
+    //booking post API
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      console.log("hit the api", booking);
 
-        //booking get API
-        app.get('/booking', async(req, res) =>{
-            const cursor = bookingCollection.find({});
-            const booking = await cursor.toArray();
-            res.send(booking);
-        })
+      const result = await bookingCollection.insertOne(booking);
+      console.log(result);
+      res.json(result);
+    });
 
-        app.get("/orderEmail", (req, res) => {
-            bookingCollection
-                .find({ email: req.query.email })
-                .toArray((err, documents) => {
-                    res.send(documents);
-                });
+    //booking get API
+    app.get("/booking", async (req, res) => {
+      const cursor = bookingCollection.find({});
+      const booking = await cursor.toArray();
+      res.send(booking);
+    });
+
+    app.get("/orderEmail", (req, res) => {
+      bookingCollection
+        .find({ email: req.query.email })
+        .toArray((err, documents) => {
+          res.send(documents);
         });
-        //delete API
-        app.delete("/DeleteBookings/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await bookingCollection.deleteOne(query);
-            res.json(result);
-          });
-    }
-    finally{
-        // await client.close()
-    }
+    });
+    //Review POST API
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+
+      const result = await reviewCollection.insertOne(review);
+      // console.log(result);
+      res.json(result);
+    });
+    //Review Get API
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const review = await cursor.toArray();
+      res.send(review);
+    });
+    //delete API
+    app.delete("/DeleteBookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.json(result);
+    });
+  } finally {
+    // await client.close()
+  }
 }
 
 run().catch(console.dir);
 
-app.get('/',(req, res) =>{
-    res.send('running');
-})
+app.get("/", (req, res) => {
+  res.send("running");
+});
 
 app.listen(port, () => {
-    console.log('running port', port)
-})
+  console.log("running port", port);
+});
